f38cb747799549b1001c5fa095b73be70c86e50c
Tony <tony.saintamour@gmail.com>
2024-05-19 21:06:15 -0400

    Portfolio V1

---
+++ script.js
@@ -0,0 +1,53 @@
+const projectsContainer = document.getElementById('projects-container');
+
+const projects = [
+    {
+        name: "To Do List",
+        description: "A simple To Do list.",
+        link: "https://wizcudi.github.io/ToDoList/"
+    },
+    {
+        name: 'RPG Game',
+        description: 'Role Playing Game that allows users to fight monsters/dragons, level up and more.',
+        link: 'https://wizcudi.github.io/rpgGame/'
+    },
+    {
+        name: 'Calorie Counter',
+        description: 'Allows users to add food eaten and workouts to track there daily calories burned.',
+        link: 'https://wizcudi.github.io/calorieCounter/'
+    },
+    {
+        name: 'Music Player',
+        description: 'Allows users to play, pause, skip, and delete preloaded songs in playlist.',
+        link: 'https://wizcudi.github.io/musicPlayer/'
+    },
+    {
+        name: "Binary Convertor",
+        description: "Changes numbers & decimal numbes to binary",
+        link: "https://wizcudi.github.io/DecimalToBinary/"
+    },
+    {
+        name: 'Soccer Team Cards',
+        description: 'Displays and organizes soccer players based on criteria set by user.' ,
+        link: 'https://wizcudi.github.io/SoccerTeamCards/'
+    },
+    
+];
+
+const portfolioProjects =()=> {
+    // display portfolio projects
+    projects.forEach(({name, description, link})=>{
+        projectsContainer.innerHTML += `
+            <div class="project">
+                <a href="${link}">
+                    <h2>${name}</h2>
+                    <p>${description}</p>
+                </a>
+            </div>
+        `
+    })
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    portfolioProjects()
+})
