2a0d6f4b5840f6844407ed64ea9d72ec927a2781
Tony <tony.saintamour@gmail.com>
2024-07-27 22:29:46 -0400

    Upgrade to design

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 const projectsContainer = document.getElementById('projects-container');
 
 const projects = [
+    
     {
-        name: "To Do List",
-        description: "A simple To Do list.",
-        link: "https://wizcudi.github.io/ToDoList/"
+        name: "Shopping Cart",
+        description: "Dessert Shopping Cart",
+        link: "https://wizcudi.github.io/ShoppingCart/"
+    },
+    {
+        name: 'Pokemon Search',
+        description: 'Search up any Pokemon by name or ID number',
+        link: 'https://wizcudi.github.io/PokeDex/',
+    },
+    {
+        name: 'Calorie Counter',
+        description: 'Allows users to add food eaten and workouts to track there daily calories burned.',
+        link: 'https://wizcudi.github.io/calorieCounter/'
+    },
+    
+    {
+        name: 'Dice Game',
+        description: 'Rolling Dice Game',
+        link: 'https://wizcudi.github.io/diceGame/'
     },
     {
         name: 'RPG Game',
         description: 'Role Playing Game that allows users to fight monsters/dragons, level up and more.',
         link: 'https://wizcudi.github.io/rpgGame/'
     },
+    
+
+
     {
-        name: 'Calorie Counter',
-        description: 'Allows users to add food eaten and workouts to track there daily calories burned.',
-        link: 'https://wizcudi.github.io/calorieCounter/'
+        name: "News Author Page",
+        description: "Fetching data from Free Code Camp APi to display authors of Free Code Camp blogs",
+        link: "https://wizcudi.github.io/FCCNewsAuthorPage/"
+    },
+    {
+        name: "Forum Leader Board",
+        description: "BEST VIEWED ON MONITOR SCREEN! Fetching data from Free Code Camp to display a Forum",
+        link: "https://wizcudi.github.io/FCCForum/"
+    },
+
+    {
+        name: "To Do List",
+        description: "A simple To Do list.",
+        link: "https://wizcudi.github.io/ToDoList/"
     },
     {
         name: 'Music Player',
@@ -22,9 +53,9 @@ const projects = [
         link: 'https://wizcudi.github.io/musicPlayer/'
     },
     {
-        name: "Binary Convertor",
-        description: "Changes numbers & decimal numbes to binary",
-        link: "https://wizcudi.github.io/DecimalToBinary/"
+        name: 'Platformer Game',
+        description: 'BEST VIEWED ON MONITOR SCREEN! Mario Bros type of game with 2 check points.',
+        link: 'https://wizcudi.github.io/PlatformerGame/',
     },
     {
         name: 'Soccer Team Cards',
@@ -32,38 +63,42 @@ const projects = [
         link: 'https://wizcudi.github.io/SoccerTeamCards/'
     },
     {
-        name: 'Palindrome Checker',
-        description: 'checks palindrome for certain letters and phrases',
-        link: 'https://wizcudi.github.io/PalindromeCheck/',
-    },
-    {
-        name: 'Number Sorter',
-        description: 'Sorts numbers from least to greatest',
-        link: 'https://wizcudi.github.io/numberSorter/',
+        name: 'Phone Number Validator',
+        description: 'Determines if you typed in a valid phone number',
+        link: 'https://wizcudi.github.io/phonenumbervalid/',
     },
     {
         name: 'Statistics Calculator',
         description: 'Performing statistical calculations like mean, median, mode, variance, and standard deviation.',
         link: 'https://wizcudi.github.io/statisticsCalculator/',
     },
-    {
-        name: 'Roman Numeral Converter',
-        description: 'Converts to Roman Numerals',
-        link: 'https://wizcudi.github.io/RomanNumeralConverter/',
-    },
     
 ];
+
+const toggleNavbar = () => {
+
+    // checks current display style
+    // if it is 'flex' it sets to none
+    // if its not 'flex' it will set to flex
+    // CSS automactically sets to none, but will also display toggle button
+    // pressing toggle button can active function 
+    const navbarLinks = document.getElementById("navbar-links");
+    if (navbarLinks.style.display === "flex") {
+        navbarLinks.style.display = "none";
+    } else {
+        navbarLinks.style.display = "flex";
+    }
+
+}
 
 const portfolioProjects =()=> {
     // display portfolio projects
     projects.forEach(({name, description, link})=>{
         projectsContainer.innerHTML += `
-            <div class="project">
-                <a href="${link}">
-                    <h2>${name}</h2>
-                    <p>${description}</p>
-                </a>
-            </div>
+            <a class="project" href="${link}">
+                <h2>${name}</h2>
+                <p>${description}</p>
+            </a>
         `
     })
 }
